aa4cb558562974573aeabb0e56a3a900efffa370
picolloo <lucaspicolloo@gmail.com>
2020-08-02 18:00:41 -0300

    feat: News

---
+++ src/components/News/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface NewsCard {
+  title?: string;
+  date?: string;
+  description?: string;
+  href?: string;
+}
+
+const NewsCard = ({
+  title,
+  date,
+  description,
+  href,
+}: NewsCard): React.ReactElement => (
+  <div className="bg-white shadow-md rounded-lg w-full mt-2 sm:mt-0 sm:w-72 max-w-md">
+    <div className="px-4 py-2 mt-2">
+      <h3 className="font-bold text-md text-yellow-300 tracking-normal">
+        My Journey to the Mountains.
+      </h3>
+      <small className="text-gray-400">20/04/2020 - Florianópolis</small>
+
+      <p className="text-sm text-gray-700 mt-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        reiciendis ad architecto at aut placeat quia, minus dolor.
+      </p>
+
+      <div className="flex justify-end mt-2">
+        <button className="font-semibold text-yellow-300 hover:opacity-75 focus:outline-none ">
+          Saiba mais
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+const News: React.FC = () => {
+  return (
+    <div className="md:container mx-auto p-4">
+      <h2 className="text-yellow-300 font-semibold text-2xl">Notícias</h2>
+      <div className="flex flex-col sm:flex-wrap sm:flex-row justify-between items-center mt-4">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </div>
+    </div>
+  );
+};
+
+export default News;
