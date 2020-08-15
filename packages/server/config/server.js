21952d3bb0ee5dd0432d24bfae3a33c62a99a975
picolloo <lucaspicolloo@gmail.com>
2020-08-15 00:14:50 -0300

    feat: server structure with Strapi

---
+++ packages/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef785ba31b4450f82fda4cd78d4148c4'),
+    },
+  },
+});
---
+++ packages/server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bed004cc-f0c4-4ccc-b791-844b74f87dea'
+};
